e7dc6b1a755246a76062032ecafb8e363bf839f2
RoninXYZ <xyzronin97@gmail.com>
2022-09-03 13:31:48 +0700

    jenis jenis json

---
+++ contoh1/app.js
@@ -0,0 +1,4 @@
+// const txt = '{"name":"John", "age":30, "city":"New York"}'
+// const obj = JSON.parse(txt);
+// document.getElementById("demo").innerHTML = obj.name + ", " + obj.age;
+//json diubah menjadi javascript object
